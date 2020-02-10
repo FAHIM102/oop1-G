d58ba1a2f1d30de4b47ef75611ee38fa1a84bc94
SADMAN SHAKIB AKIB <60264171+AKIB116@users.noreply.github.com>
2020-02-10 10:48:04 +0600

    Read me

---
+++ Rectangle.java
@@ -0,0 +1,39 @@
+
+public class Rectangle {
+	private double height;
+	private double width;
+	
+	public Rectangle() {
+		
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	public double getWidth() {
+		return width;
+	}
+
+	public void setWidth(double width) {
+		this.width = width;
+	}
+
+	@Override
+	public String toString() {
+		return "Rectangle [height=" + height + ", width=" + width + "]";
+	}
+
+	public Rectangle(double height, double width) {
+		super();
+		this.height = height;
+		this.width = width;
+	}
+	public double CalculateArea() {
+		return (height*width);
+	}
+}
---
+++ Rectest.java
@@ -0,0 +1,9 @@
+
+public class Rectest {
+public static void main (String[]args) {
+	Rectangle r1=new Rectangle(5,4);
+	System.out.println(r1);
+	System.out.println(r1.CalculateArea());
+}
+
+}
